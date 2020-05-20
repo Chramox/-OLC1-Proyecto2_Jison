7a4f790a67f52baf1126c27d16a18b354b9a91c0
Chramox <2994775760101@ingenieria.usac.edu.gt>
2020-05-19 23:13:44 -0600

    Modificando interfaz

--- FrontEnd/server_http.go
+++ FrontEnd/server_http.go
@@ -7,16 +7,17 @@ import(
 )
 
 func index(w http.ResponseWriter, r *http.Request)  {
-	template, err := template.ParseFiles("../FrontEnd/Interfaz.html")
+	template, err := template.ParseFiles("Interfaz.html")
 	if err != nil {
 		fmt.Fprintf(w, "PAGINA NO ENCONTRADA")
 	}else{
 		template.Execute(w,nil)
 	}
 }
-
+//TODO: QUE EL GO JALE EL ACE EDITOR
 func main()  {
 	http.HandleFunc("/", index)
 	fmt.Println("HOla mundo")
 	http.ListenAndServe(":8080",nil)
+
 }
